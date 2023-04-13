b9432dec37a45756e521f1c5064700e52745083d
Ata <ataserb@gmail.com>
2023-04-13 20:34:19 +0300

    done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((total, side) => total + side);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    const [a, b, c] = this.sides;
+    return a + b > c && b + c > a && a + c > b;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.sides.every((side) => side === this.sides[0]);
+  }
+
+  get area() {
+    return this.sides[0] ** 2;
+  }
+}
